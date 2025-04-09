4de35b75096dddbad4fdc17aa71cb210f079ef68
dredknight <gangstarrr@abv.bg>
2025-04-09 18:29:17 +0300

    RC19e version

--- convert_UTF8.ps1
+++ convert_UTF8.ps1
@@ -1,15 +1,17 @@
-$directory = "$PSScriptRoot\mod\"
+$directory = "$PSScriptRoot\MMH55-Texts-ES\"
 Write-Output "Processing directory..."
 
 # - UTF-8 is used by this repository so github can show diff during PRs
 $encoding = [System.Text.Encoding]::UTF8
 
 Get-ChildItem -Path $directory -Filter "*.txt" -Recurse | ForEach-Object {
-	$inputFile = $_.FullName
-	Write-Output "Converting: $inputFile"
+    $inputFile = $_.FullName
+    Write-Output "Converting: $inputFile"
 
-	## Read the file and write it back in UTF-8
-	$content = Get-Content -Raw -Path $inputFile
-	[System.IO.File]::WriteAllText($inputFile, $content, $encoding)
+    if ($_.IsReadOnly) { $_.IsReadOnly = $false }
+
+    ## Read the file and write it back in UTF-8
+    $content = Get-Content -Raw -Path $inputFile
+    [System.IO.File]::WriteAllText($inputFile, $content, $encoding)
 }
 Write-Output "Conversion complete!"
